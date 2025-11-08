d485fe8fa6e0792c10e8696cbb59dcff956bb01e
davidras284-blip <davidras284@gmail.com>
2025-11-08 15:51:40 -0700

    1st Commit of the Course

---
+++ hola_mundo.py
@@ -0,0 +1,9 @@
+print("hola_mundo")
+# This is a simple Python script that prints "hola_mundo" to the console.   
+# It serves as a basic example of how to output text in Python.
+# To run this script, save it as hola_mundo.py and execute it using a Python interpreter.
+# Example:
+# python hola_mundo.py
+# Expected Output:
+# hola_mundo
+# Enjoy coding!
